70de0fe6c83a3d293b9fbdeeda0e2742a52bf359
anastasia <dinalis13@gmail.com>
2023-02-09 13:07:27 +0200

    to correct a grammatical error in print

--- usd_yuan.py
+++ usd_yuan.py
@@ -18,16 +18,18 @@ if __name__ == "__main__":
                 print(f'Date: 04.02.2023\n'
                       f'currency exchange rate: 0.15')
                 print(f'{amount} CNY in USD = {(float(amount) * YUAN_TO_USD):.2f} USD')
+
                 break
+
             elif number > 2:
-                print(f'bbTry again. Enter the numbers "1" or "2 ')
+                print(f'Try again. Enter the numbers "1" or "2 ')
 
             else:
-                number = int(input("bbNumber: "))
+                number = int(input("Number: "))
                 amount = float(input("Please enter an amount for the conversion: "))
 
         except:
-            print(f'Try again. Enter the numbers "1" or "2 ')
+            print(f'Try again. Enter the numbers "1" or "2" ')
 
 
 
